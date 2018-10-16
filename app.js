d49919b504a4774f4ae9a5ac5b83a195edfabe43
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-10-16 13:21:07 -0700

    users can edit and update charities

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
@@ -15,6 +16,7 @@ const Charity = mongoose.model('Charity', {
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 // let charities = [
 //   { org: "Humane Society" },
 //   { org: "Childrens Hospital" },
@@ -42,7 +44,7 @@ app.get('/charities/new', (req, res) => {
   res.render('charities-new', {});
 })
 
-app.post('/chairities', (req, res) => {
+app.post('/charities', (req, res) => {
     Charity.create(req.body).then((charity) => {
         console.log(charity)
         res.redirect(`/charities/${charity._id}`)
@@ -58,6 +60,22 @@ app.get('/charities/:id', (req, res) => {
       console.log(err.message)
   })
 });
+
+app.get('/charities/:id/edit', (req, res) => {
+    Charity.findById(req.params.id, function(err, charity) {
+        res.render('charities-edit', {charity: charity})
+    })
+})
+
+app.put('/charities/:id', (req, res) => {
+    Charity.findByIdAndUpdate(req.params.id, req.body)
+        .then(charity => {
+            res.redirect(`/charities/${charity._id}`)
+        })
+        .catch(err => {
+            console.log(err.message)
+        })
+})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')

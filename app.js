f82c7864797afd8b9ab90e79da756fbc9484be4b
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-10-16 11:24:14 -0700

    added mongodb & charity model

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/charity-tracker');
 
 var exphbs = require('express-handlebars');
+
+
+const Charity = mongoose.model('Charity', {
+  org: String
+});
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-let charities = [
-  { charity: "Humane Society" },
-  { charity: "Childrens Hospital" },
-  { charity: "Childrens Hospital" },
-  { charity: "Childrens Hospital" },
-  { charity: "Childrens Hospital" }
-]
+// let charities = [
+//   { org: "Humane Society" },
+//   { org: "Childrens Hospital" },
+//   { org: "Childrens Hospital" },
+//   { org: "Childrens Hospital" },
+//   { org: "Childrens Hospital" }
+// ]
 
 // INDEX
+// app.get('/', (req, res) => {
+//   res.render('charities-index', { charities: charities });
+// })
+
 app.get('/', (req, res) => {
-  res.render('charities-index', { charities: charities });
+    Charity.find()
+        .then(charities => {
+            res.render('charities-index', {charities: charities})
+        })
+        .catch(err => {
+            console.log(err)
+        })
 })
 
 app.listen(3000, () => {
